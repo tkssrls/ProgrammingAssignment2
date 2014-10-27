195f4102368239fa556fd5e66d876baeed8660cb
tkssrls <panagiotis.sereleas@accenture.com>
2014-10-27 02:00:23 +0200

    test
    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
 ## Write a short comment describing this function
 
